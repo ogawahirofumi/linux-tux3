8f64ee86c870c6c7fe7c86585e914cf98e37b1ba
OGAWA Hirofumi <hirofumi@mail.parknet.co.jp>
2014-10-13 02:00:39 +0900

    tux3: Add simple seq_file to get current mount options
    Signed-off-by: OGAWA Hirofumi <hirofumi@mail.parknet.co.jp>

---
+++ fs/tux3/user/libklib/seq_file.c
@@ -0,0 +1,63 @@
+/*
+ * lib/parser.c - simple seq_file emulation
+ *
+ * This source code is licensed under the GNU General Public License,
+ * Version 2.  See the file COPYING for more details.
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <libklib/seq_file.h>
+
+static void seq_set_overflow(struct seq_file *m)
+{
+	m->count = m->size;
+}
+
+int seq_vprintf(struct seq_file *m, const char *f, va_list args)
+{
+	int len;
+
+	if (m->count < m->size) {
+		len = vsnprintf(m->buf + m->count, m->size - m->count, f, args);
+		if (m->count + len < m->size) {
+			m->count += len;
+			return 0;
+		}
+	}
+	seq_set_overflow(m);
+	return -1;
+}
+
+int seq_printf(struct seq_file *m, const char *f, ...)
+{
+	int ret;
+	va_list args;
+
+	va_start(args, f);
+	ret = seq_vprintf(m, f, args);
+	va_end(args);
+
+	return ret;
+}
+
+int seq_putc(struct seq_file *m, char c)
+{
+	if (m->count < m->size) {
+		m->buf[m->count++] = c;
+		return 0;
+	}
+	return -1;
+}
+
+int seq_puts(struct seq_file *m, const char *s)
+{
+	int len = strlen(s);
+	if (m->count + len < m->size) {
+		memcpy(m->buf + m->count, s, len);
+		m->count += len;
+		return 0;
+	}
+	seq_set_overflow(m);
+	return -1;
+}
---
+++ fs/tux3/user/libklib/seq_file.h
@@ -0,0 +1,31 @@
+#ifndef LIBKLIB_SEQ_FILE_H
+#define LIBKLIB_SEQ_FILE_H
+
+#include <stdarg.h>
+#include <stdbool.h>
+#include <libklib/types.h>
+
+struct seq_file {
+	char *buf;
+	size_t size;
+	size_t count;
+	void *private;
+};
+
+/*
+ * seq_files have a buffer which can may overflow. When this happens a larger
+ * buffer is reallocated and all the data will be printed again.
+ * The overflow state is true when m->count == m->size.
+ */
+static inline bool seq_overflow(struct seq_file *m)
+{
+	return m->count == m->size;
+}
+
+int seq_putc(struct seq_file *m, char c);
+int seq_puts(struct seq_file *m, const char *s);
+
+__printf(2, 3) int seq_printf(struct seq_file *, const char *, ...);
+__printf(2, 0) int seq_vprintf(struct seq_file *, const char *, va_list args);
+
+#endif /* !LIBKLIB_SEQ_FILE_H */
