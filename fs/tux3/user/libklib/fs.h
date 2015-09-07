89e74e4163989f8cc2bd352f2d543c989dc8763e
OGAWA Hirofumi <hirofumi@mail.parknet.co.jp>
2015-09-08 04:35:30 +0900

    tux3: Add struct kstatfs to consolidate tux3_statfs()
    Signed-off-by: OGAWA Hirofumi <hirofumi@mail.parknet.co.jp>

--- fs/tux3/user/libklib/fs.h
+++ fs/tux3/user/libklib/fs.h
@@ -12,6 +12,25 @@
 struct nameidata {
 };
 
+typedef struct {
+	int	val[2];
+} __kernel_fsid_t;
+
+struct kstatfs {
+	long f_type;
+	long f_bsize;
+	u64 f_blocks;
+	u64 f_bfree;
+	u64 f_bavail;
+	u64 f_files;
+	u64 f_ffree;
+	__kernel_fsid_t f_fsid;
+	long f_namelen;
+	long f_frsize;
+	long f_flags;
+	long f_spare[4];
+};
+
 /*
  * Attribute flags.  These should be or-ed together to figure out what
  * has been changed!
