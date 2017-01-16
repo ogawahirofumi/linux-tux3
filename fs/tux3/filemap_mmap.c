ed0070d4f303de1fc7ea119ad878195426e241ef
OGAWA Hirofumi <hirofumi@mail.parknet.co.jp>
2017-01-16 23:19:31 +0900

    tux3: Kill ->remap_pages for v4.9
    Signed-off-by: OGAWA Hirofumi <hirofumi@mail.parknet.co.jp>

--- fs/tux3/filemap_mmap.c
+++ fs/tux3/filemap_mmap.c
@@ -244,7 +244,6 @@ static const struct vm_operations_struct tux3_file_vm_ops = {
 	.fault		= filemap_fault,
 	.map_pages	= filemap_map_pages,
 	.page_mkwrite	= tux3_page_mkwrite,
-	.remap_pages	= generic_file_remap_pages,
 };
 
 int tux3_file_mmap(struct file *file, struct vm_area_struct *vma)
