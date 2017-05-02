1cd514ab243ec1bec4e01ce0fe43bb5d4af499e9
OGAWA Hirofumi <hirofumi@mail.parknet.co.jp>
2017-05-02 14:13:02 +0900

    tux3: Update libklib/err.h for v4.11
    Signed-off-by: OGAWA Hirofumi <hirofumi@mail.parknet.co.jp>

--- fs/tux3/user/libklib/err.h
+++ fs/tux3/user/libklib/err.h
@@ -6,31 +6,39 @@
 /* Ripped from Linux Kernel by D.Phillips, GPL v2 */
 
 #define MAX_ERRNO       4095
-#define IS_ERR_VALUE(x) ((x) >= (unsigned long)-MAX_ERRNO)
+#define IS_ERR_VALUE(x) unlikely((unsigned long)(void *)(x) >= (unsigned long)-MAX_ERRNO)
 
 static inline void * __must_check ERR_PTR(long error)
 {
-	return (void *)error;
+	return (void *) error;
 }
 
-static inline long __must_check PTR_ERR(const void *ptr)
+static inline long __must_check PTR_ERR(__force const void *ptr)
 {
-	return (long)ptr;
+	return (long) ptr;
 }
 
-static inline bool __must_check IS_ERR(const void *ptr)
+static inline bool __must_check IS_ERR(__force const void *ptr)
 {
 	return IS_ERR_VALUE((unsigned long)ptr);
 }
 
-static inline bool __must_check IS_ERR_OR_NULL(const void *ptr)
+static inline bool __must_check IS_ERR_OR_NULL(__force const void *ptr)
 {
-	return !ptr || IS_ERR_VALUE((unsigned long)ptr);
+	return unlikely(!ptr) || IS_ERR_VALUE((unsigned long)ptr);
 }
 
-static inline void * __must_check ERR_CAST(const void *ptr)
+/**
+ * ERR_CAST - Explicitly cast an error-valued pointer to another pointer type
+ * @ptr: The pointer to cast.
+ *
+ * Explicitly cast an error-valued pointer to another pointer type in such a
+ * way as to make it clear that's what's going on.
+ */
+static inline void * __must_check ERR_CAST(__force const void *ptr)
 {
-	return (void *)ptr;
+	/* cast away the const */
+	return (void *) ptr;
 }
 
 static inline int __must_check PTR_ERR_OR_ZERO(__force const void *ptr)
