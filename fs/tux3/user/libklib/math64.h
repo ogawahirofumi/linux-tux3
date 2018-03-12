8a9b666627b378b33ab919ab0cb2caa905841df9
OGAWA Hirofumi <hirofumi@mail.parknet.co.jp>
2018-03-12 22:46:36 +0900

    tux3: Update libklib/math64 for v4.15
    Signed-off-by: OGAWA Hirofumi <hirofumi@mail.parknet.co.jp>

--- fs/tux3/user/libklib/math64.h
+++ fs/tux3/user/libklib/math64.h
@@ -3,6 +3,14 @@
 
 #include <libklib/types.h>
 
+/**
+ * div_s64_rem - signed 64bit divide with 32bit divisor with remainder
+ * @dividend: signed 64bit dividend
+ * @divisor: signed 32bit divisor
+ * @remainder: pointer to signed 32bit remainder
+ *
+ * Return: sets ``*remainder``, then returns dividend / divisor
+ */
 static inline s64 div_s64_rem(s64 dividend, s32 divisor, s32 *remainder)
 {
 	*remainder = dividend % divisor;
