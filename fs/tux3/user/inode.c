8bf9495e221dff446da4bae2d4b53af8c33ced61
OGAWA Hirofumi <hirofumi@mail.parknet.co.jp>
2018-08-17 10:25:52 +0900

    tux3: Add inode_insert5()
    Signed-off-by: OGAWA Hirofumi <hirofumi@mail.parknet.co.jp>

--- fs/tux3/user/inode.c
+++ fs/tux3/user/inode.c
@@ -170,13 +170,23 @@ static struct inode *find_inode(struct super_block *sb, struct hlist_head *head,
 				int (*test)(struct inode *, void *),
 				void *data)
 {
-	struct inode *inode;
+	struct inode *inode = NULL;
 
+repeat:
 	hlist_for_each_entry(inode, head, i_hash) {
-		if (test(inode, data)) {
-			__iget(inode);
-			return inode;
+		if (inode->i_sb != sb)
+			continue;
+		if (!test(inode, data))
+			continue;
+		spin_lock(&inode->i_lock);
+		if (inode->i_state & (I_FREEING|I_WILL_FREE)) {
+			assert(0);	/* On userland, shouldn't happen */
+			spin_unlock(&inode->i_lock);
+			goto repeat;
 		}
+		__iget(inode);
+		spin_unlock(&inode->i_lock);
+		return inode;
 	}
 	return NULL;
 }
@@ -196,8 +206,57 @@ static struct inode *ilookup5(struct super_block *sb, inum_t inum,
 		int (*test)(struct inode *, void *), void *data)
 {
 	struct inode *inode = ilookup5_nowait(sb, inum, test, data);
-	/* On userland, inode shouldn't have I_NEW */
-	assert(!inode || !(inode->i_state & I_NEW));
+again:
+	if (inode) {
+		/* On userland, inode shouldn't have I_NEW */
+		assert(!(inode->i_state & I_NEW));
+		//wait_on_inode(inode);
+		if (unlikely(inode_unhashed(inode))) {
+			iput(inode);
+			goto again;
+		}
+	}
+	return inode;
+}
+
+static struct inode *inode_insert5(struct inode *inode, inum_t inum,
+			    int (*test)(struct inode *, void *),
+			    int (*set)(struct inode *, void *), void *data)
+{
+	struct hlist_head *head = inode_hashtable + hash(inum);
+	struct inode *old;
+
+again:
+	old = find_inode(inode->i_sb, head, test, data);
+	if (unlikely(old)) {
+		/*
+		 * Uhhuh, somebody else created the same inode under us.
+		 * Use the old inode instead of the preallocated one.
+		 */
+		assert(!(inode->i_state & I_NEW));
+		//wait_on_inode(old);
+		if (unlikely(inode_unhashed(old))) {
+			iput(old);
+			goto again;
+		}
+		return old;
+	}
+
+	if (set && unlikely(set(inode, data))) {
+		inode = NULL;
+		goto unlock;
+	}
+
+	/*
+	 * Return the locked inode with I_NEW set, the
+	 * caller is responsible for filling in the contents
+	 */
+	spin_lock(&inode->i_lock);
+	inode->i_state |= I_NEW;
+	hlist_add_head(&inode->i_hash, head);
+	spin_unlock(&inode->i_lock);
+unlock:
+
 	return inode;
 }
 
@@ -205,55 +264,30 @@ static struct inode *iget5_locked(struct super_block *sb, inum_t inum,
 			   int (*test)(struct inode *, void *),
 			   int (*set)(struct inode *, void *), void *data)
 {
-	struct hlist_head *head = inode_hashtable + hash(inum);
-	struct inode *inode;
+	struct inode *inode = ilookup5(sb, inum, test, data);
 
-	inode = find_inode(sb, head, test, data);
-	if (inode)
-		return inode;
+	if (!inode) {
+		struct inode *new = new_inode(sb);
 
-	inode = new_inode(sb);
-	if (!inode)
-		return NULL;
-	if (set(inode, data)) {
-		destroy_inode(inode);
-		return NULL;
+		if (new) {
+			inode = inode_insert5(new, inum, test, set, data);
+			if (unlikely(inode != new))
+				iput(new);
+		}
 	}
-
-	inode->i_state = I_NEW;
-	hlist_add_head(&inode->i_hash, head);
-
 	return inode;
 }
 
 static int insert_inode_locked4(struct inode *inode, inum_t inum,
 			 int (*test)(struct inode *, void *), void *data)
 {
-	struct hlist_head *head = inode_hashtable + hash(inum);
+	struct inode *old = inode_insert5(inode, inum, test, NULL, data);
 
-	while (1) {
-		struct inode *old = NULL;
-
-		hlist_for_each_entry(old, head, i_hash) {
-			if (!test(old, data))
-				continue;
-			if (old->i_state & (I_FREEING /*|I_WILL_FREE*/))
-				continue;
-			break;
-		}
-		if (likely(!old)) {
-			inode->i_state |= I_NEW;
-			hlist_add_head(&inode->i_hash, head);
-			return 0;
-		}
-		__iget(old);
-		/* wait_on_inode(old); */
-		if (unlikely(!inode_unhashed(old))) {
-			iput(old);
-			return -EBUSY;
-		}
+	if (old != inode) {
 		iput(old);
+		return -EBUSY;
 	}
+	return 0;
 }
 
 loff_t i_size_read(const struct inode *inode)
