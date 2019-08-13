1e37309e09ca08d3dc2f98508636524044148a3e
OGAWA Hirofumi <hirofumi@mail.parknet.co.jp>
2019-08-13 23:43:26 +0900

    tux3: Update math64.h for v5.2
    Signed-off-by: OGAWA Hirofumi <hirofumi@mail.parknet.co.jp>

--- fs/tux3/user/libklib/math64.h
+++ fs/tux3/user/libklib/math64.h
@@ -17,4 +17,23 @@ static inline s64 div_s64_rem(s64 dividend, s32 divisor, s32 *remainder)
 	return dividend / divisor;
 }
 
+static __always_inline u32
+__iter_div_u64_rem(u64 dividend, u32 divisor, u64 *remainder)
+{
+	u32 ret = 0;
+
+	while (dividend >= divisor) {
+		/* The following asm() prevents the compiler from
+		   optimising this loop into a modulo operation.  */
+		asm("" : "+rm"(dividend));
+
+		dividend -= divisor;
+		ret++;
+	}
+
+	*remainder = dividend;
+
+	return ret;
+}
+
 #endif /* !LIBKLIB_MATH64_H */
