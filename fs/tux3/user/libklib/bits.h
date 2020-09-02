08d3053da9c36c9e6945bc2022d137da4277d455
OGAWA Hirofumi <hirofumi@mail.parknet.co.jp>
2020-09-02 20:36:32 +0900

    tux3: Update libklib/bits.h for v5.8
    Signed-off-by: OGAWA Hirofumi <hirofumi@mail.parknet.co.jp>

--- fs/tux3/user/libklib/bits.h
+++ fs/tux3/user/libklib/bits.h
@@ -19,8 +19,7 @@
  * position @h. For example
  * GENMASK_ULL(39, 21) gives us the 64bit vector 0x000000ffffe00000.
  */
-#if !defined(__ASSEMBLY__) && \
-	(!defined(__GNUC__) || GCC_VERSION >= 49000)
+#if !defined(__ASSEMBLY__)
 #include <libklib/build_bug.h>
 #define GENMASK_INPUT_CHECK(h, l) \
 	(BUILD_BUG_ON_ZERO(__builtin_choose_expr( \
